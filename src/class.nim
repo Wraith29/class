6107096ae6b0319fa73568923e193733b12199ca
Isaac Naylor <I.acnaylor@gmail.com>
2022-10-02 23:40:48 +0100

    Initial Commit

---
+++ src/class.nim
@@ -0,0 +1,37 @@
+import macros
+
+template static* {.pragma.}
+
+macro class*(head, body: untyped): untyped =
+  if head.kind != nnkIdent: return
+  result = newStmtList()
+  let typeName = head.strVal
+  var attrs = newNimNode(nnkRecList)
+  let typeDef = newNimNode(nnkTypeSection)
+    .add(newNimNode(nnkTypeDef)
+      .add(ident(typeName))
+      .add(newEmptyNode())
+      .add(newNimNode(nnkRefTy)
+        .add(newNimNode(nnkObjectTy)
+          .add(newEmptyNode())
+          .add(newEmptyNode())
+          .add(attrs)
+        )
+      )
+    )
+
+  result.add(typeDef)
+
+  for statement in body.children:
+    if statement.kind == nnkCall:
+      attrs.add(
+        newNimNode(nnkIdentDefs)
+          .add(ident(statement[0].strVal))
+          .add(ident(statement[1][0].strVal))
+          .add(newEmptyNode())
+      )
+    elif statement.kind in [nnkProcDef, nnkFuncDef]:
+      var def = statement
+      if def[4].kind == nnkEmpty:
+        def[3][1][1] = ident(head.strVal)
+      result.add(def)
